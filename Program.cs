194fe823c69017ed9a6ddd49fa8ae1e20f303506
HasanAbbas2009 <hasanabbaskazim@gmail.com>
2023-12-18 21:47:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class FibonacciSeries
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter the number of terms for the Fibonacci series: ");
+        int numTerms = int.Parse(Console.ReadLine());
+
+       
+        Console.WriteLine("Fibonacci Series:");
+        DisplayFibonacciSeries(numTerms);
+    }
+
+    
+    private static void DisplayFibonacciSeries(int numTerms)
+    {
+        int firstTerm = 0, secondTerm = 1;
+
+        for (int i = 0; i < numTerms; i++)
+        {
+            Console.Write(firstTerm + " ");
+
+            
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
